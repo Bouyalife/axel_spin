9c62b0b0854018db01d46c8bd1a472b2547240a3
Raffi <Raffi@DESKTOP-86NFDVV>
2023-05-06 12:54:01 +0200

    boopitybapedy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Wheel } from 'react-custom-roulette'
 
-function App() {
+
+export default  ()  => {
+ 
+    // All names in the current wheel
+  const[array, setArray] = useState([
+  ]);
+
+  const WebSocket = window.WebSocket;
+  const [socket, setSocket] = useState(null);
+
+  // Current input
+  const [name, setName] = useState("");
+  
+  // If wheel is spinning
+  const [mustSpin, setMustSpin] = useState(false);
+  // Winning index
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  useEffect(()=>{
+    const socket = new WebSocket("ws://axelspin-backend.onrender.com");
+
+    socket.onopen = () =>
+    {
+      setSocket(socket);
+      console.log("Connected to server");
+    }
+    
+    socket.onmessage = (resp) =>{
+      console.log(resp.data);
+      var splitMessage = resp.data.split(" ");
+      if(splitMessage[0] === "spin")
+      {
+        setPrizeNumber(splitMessage[1]);
+        setMustSpin(true);
+      }
+      if (splitMessage[0] === "name") 
+      {
+        setArray(oldArray => [...oldArray, {option: splitMessage[1]}]);
+      }
+      if(splitMessage[0] === "delete")
+      {
+        setArray(oldArray => oldArray.filter((index) => index.option !== splitMessage[1]));
+      }
+      if(splitMessage[0] === "names")
+      {
+        for(let i = 1; i < splitMessage.length; i++)
+        {
+          setArray(oldArray => [...oldArray, {option: splitMessage[i]}]);
+        }
+      }
+    }
+    return () => {
+      socket.close();
+    };
+  },[]);
+
+  const handleKeyDown = (event) =>
+  {
+    if(event.key === 'Enter')
+    {
+      socket.send("name " + name);
+      setName("");
+    }
+  }
+
+  function handleChange (event) {
+    setName(event.target.value);
+  }
+
+  function deleteName (event) {
+    socket.send("delete " + event.target.value);
+  }
+
+  const spinWheel = () =>
+  {
+    socket.send("spin");
+  }
+
+  const resetWheel = () =>
+  {
+    setMustSpin(false);
+    socket.send("spindone");
+  }
+
+  const winner = array.find(item => item.option === prizeNumber);
+  const winnerOption = winner ? winner.option : '';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {array.length > 0 ? (<div>
+    <Wheel
+      mustStartSpinning={mustSpin}
+      prizeNumber={prizeNumber}
+      data={array}
+      backgroundColors={['#3e3e3e', '#df3428']}
+      textColors={['#ffffff']}
+      onStopSpinning={resetWheel}
+    /></div>)
+    : null}
+    
+    {mustSpin ? null : <div>
+    <button onClick={spinWheel}>Spin!</button>
+      {array.map(index => <button value={index.option} onClick={deleteName}>{index.option}</button>)}
+      <input type="text" onChange={handleChange} onKeyDown={handleKeyDown} value={name} placeholder='Enter name!'/>
+      {array.length}
+    </div> }
+    
+  </>
   );
 }
-
-export default App;
